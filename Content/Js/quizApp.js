f2fe870aee27f06b4c4929a42a6f3a2e5b24158d
stefankarlsson <karlssonstefan88@gmail.com>
2015-11-11 15:23:24 +0100

    First commit, basic webapp in place

---
+++ Content/Js/quizApp.js
@@ -0,0 +1,57 @@
+﻿(function () {
+	var app = angular.module("quiz", []);
+
+	app.controller("QuestionController", ['$scope', function($scope){
+		$scope.questions = questions.shuffle();
+		$scope.currentQuestionIndex = 0;
+		$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+		$scope.numberOfCorrectAnswers = 0;
+		$scope.failedQuestions = [];
+		$scope.isQuizFinished = false;
+		$scope.progressPercentage =0;					
+
+		$scope.submitAnswer = function(answer){			
+			if(answer.isRightAnswer){
+				$scope.numberOfCorrectAnswers++;
+			} else {
+				$scope.currentQuestion.userAnswer = answer;
+				$scope.failedQuestions.push($scope.currentQuestion);
+			}
+
+			$scope.currentQuestionIndex++;
+
+			if($scope.currentQuestionIndex === $scope.questions.length){
+				$scope.isQuizFinished = true;
+				$scope.currentQuestionIndex = 0;
+			}
+
+			$scope.setCurrentQuestion();
+			$scope.progressPercentage = $scope.getProgressPercentage();			
+			window.scrollTo(0, 0);
+		};
+
+		$scope.setCurrentQuestion = function(){
+			$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+		};
+
+		$scope.restartQuiz = function(){
+			$scope.questions.shuffle();
+			for(var i = 0; i < $scope.questions.length; i++){
+				$scope.questions[i].answers.shuffle();
+			};
+			$scope.isQuizFinished = false;
+			$scope.currentQuestionIndex = 0;
+			$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+			$scope.numberOfCorrectAnswers = 0;
+			$scope.failedQuestions = [];
+			window.scrollTo(0, 0);
+		};
+
+		$scope.getProgressPercentage = function(){
+			return Math.floor($scope.currentQuestionIndex / 40 * 100);
+		};
+
+	}]);
+
+})();
+
