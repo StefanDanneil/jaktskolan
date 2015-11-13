85ff8d1df1ce026a70ce0800e25243edfea12d74
stefankarlsson <karlssonstefan88@gmail.com>
2015-11-14 00:36:27 +0100

    Added landingpage and lightning-question functionality

--- Content/Js/quizApp.js
+++ Content/Js/quizApp.js
@@ -8,30 +8,73 @@
 		$scope.numberOfCorrectAnswers = 0;
 		$scope.failedQuestions = [];
 		$scope.isQuizFinished = false;
-		$scope.progressPercentage =0;
+		$scope.progressPercentage = 0;
+		$scope.lightningQuestionsEnabled = false;
+		$scope.showLandingPage = true;
+		$scope.isClickEnabled = true;
 
-		$scope.submitAnswer = function(answer){
-			if(answer.isRightAnswer){
-				$scope.numberOfCorrectAnswers++;
-			} else {
-				$scope.currentQuestion.userAnswer = answer;
-				$scope.failedQuestions.push($scope.currentQuestion);
-			}
-
+		$scope.submitAnswer = function(answer, event){
 			$scope.currentQuestionIndex++;
 
-			if($scope.currentQuestionIndex === $scope.questions.length){
-				$scope.isQuizFinished = true;
-				$scope.currentQuestionIndex = 0;
+			if($scope.lightningQuestionsEnabled) {
+				if(answer.isRightAnswer){
+					var triggeredButton = $(event.target);
+					triggeredButton.removeClass('btn-primary').addClass('btn-success');
+					$scope.isClickEnabled = false;
+					setTimeout(function(){
+						if($scope.currentQuestionIndex === 20){
+							$scope.questions.shuffle();
+							$scope.currentQuestionIndex = 0;
+						}
+						$scope.isClickEnabled = true;
+						$scope.setCurrentQuestion();
+						$scope.$apply();
+					}, 500);
+				} else {
+					var triggeredButton = $(event.target);
+					var rightAnswer = $();
+					triggeredButton.removeClass('btn-primary').addClass('animated shake btn-danger');
+					$('.btn[data-isRightAnswer="true"]').removeClass('btn-primary').addClass('animated pulse btn-success');
+					$scope.isClickEnabled = false;
+					setTimeout(function(){
+						if($scope.currentQuestionIndex === 20){
+							$scope.questions.shuffle();
+							$scope.currentQuestionIndex = 0;
+						}
+
+						$scope.isClickEnabled = true;
+						$scope.setCurrentQuestion();
+						$scope.$apply();
+					}, 2000);
+				}
+
+			} else {
+				if(answer.isRightAnswer){
+					$scope.numberOfCorrectAnswers++;
+				} else {
+					$scope.currentQuestion.userAnswer = answer;
+					$scope.failedQuestions.push($scope.currentQuestion);
+				}
+
+				$scope.progressPercentage = $scope.getProgressPercentage();
+
+				if($scope.currentQuestionIndex === 70){
+					$scope.isQuizFinished = true;
+					$scope.currentQuestionIndex = 0;
+				}
+
+				$scope.setCurrentQuestion();
 			}
 
-			$scope.setCurrentQuestion();
-			$scope.progressPercentage = $scope.getProgressPercentage();
 
 		};
 
 		$scope.setCurrentQuestion = function(){
-			$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+			if($scope.lightningQuestionsEnabled){
+				$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+			} else {
+				$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+			}
 			window.scrollTo(0, 0);
 		};
 
@@ -46,10 +89,21 @@
 			$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
 			$scope.numberOfCorrectAnswers = 0;
 			$scope.failedQuestions = [];
+			$scope.lightningQuestionsEnabled = false;
+			$scope.showLandingPage = false;
 		};
 
 		$scope.getProgressPercentage = function(){
 			return Math.floor($scope.currentQuestionIndex / $scope.questions.length * 100);
+		};
+
+		$scope.showToLandingPage = function(){
+			$scope.showLandingPage = true;
+		}
+
+		$scope.enableLightningQuestions = function(){
+			$scope.restartQuiz();
+			$scope.lightningQuestionsEnabled = true;
 		};
 	}]);
 })();
